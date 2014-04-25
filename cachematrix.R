7a132b1c1a64bf0fd076a027a9d57cc9ec2095ca
apikou <pikouabdel@gmail.com>
2014-04-24 21:37:08 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-##  Computing a matrix inverse can be time consuming especially for big data .The idea here is: if ##   the  matrix does not change ( and supposed to be inversible) , to write functions that can  store ##  (cache)   the first computation of the matrix inverse , to be called later.This will be done by two ##   functions ,
+##  Computing a matrix inverse can be time consuming especially for big data .The idea here is: if 
+##the  matrix does not change ( and supposed to be inversible) , to write functions that can  store 
+##  (cache)   the first computation of the matrix inverse , to be called later.This will be done by two 
+##   functions ,
 ##   makeCacheMatrix() and cacheSolve().
-##   makeCacheMatrix() creates a new object cacheMatrix that has 2 pieces of imternal data (matrix x ##   and the cached value) and 4 methodes ( 2 gets and 2 sets).
-##  cacheSolve() use output from makeCacheMatrix() ( x,get(),getInverse(), and decide either to ##calculate the the inverse or get it from cache.   
+##   makeCacheMatrix() creates a new object cacheMatrix that has 2 pieces of imternal data (matrix x 
+##   and the cached value) and 4 methodes ( 2 gets and 2 sets).
+##  cacheSolve() use output from makeCacheMatrix() ( x,get(),getInverse(), and decide either to ##calculate
+## the the inverse or get it from cache.   
 makeCacheMatrix <- function(x = matrix()) {
   cachedMatrix <- NULL
   set <- function(y) {
