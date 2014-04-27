c7a07a15a208474cc472e3c88b1850165f3baae7
pikou <pikouabdel@gmail.com>
2014-04-27 09:18:44 -0500

    text correction

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 ##  (cache)   the first computation of the matrix inverse , to be called later.This will be done by two 
 ##   functions :
 ##   makeCacheMatrix() and cacheSolve().
-##   makeCacheMatrix() creates a new object cacheMatrix that has 2 pieces of imternal data (matrix x 
+##   makeCacheMatrix() creates a new object MakecacheMatrix that has 2 pieces of imternal data (matrix x 
 ##   and the cached value) and 4 methodes ( 2 gets and 2 sets).
 ##   cacheSolve() use output from makeCacheMatrix() ( x,get(),getInverse(), and decide either to ##calculate
 ##   the the inverse or get it from cache.   
